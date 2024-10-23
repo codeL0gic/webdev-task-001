12fc9c92536327232a64ffb232feb2def9f89c72
codeL0gic <shannon.neumann@yum.com>
2024-10-23 14:04:45 -0400

    webdev-task-001

---
+++ js/html.bat
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+        <meta http-equiv="X-UA-Compatible" content"ie=edge">
+        <title>Web Development Vocabulary</title>
+
+        <link rel="stylesheet" href="style.css"/>
+
+</head>
+
+<body>
+    <section>
+        <main>
+   
+    <header>
+        <h1>Web Development Vocabulary</h1>
+    </header>
+    
+    <ul>
+    
+    <li><b>API:</b> A set of functions and tools for building software applications, often interacting with external systems.</li>
+    <li><b>Async/Await:</b> Syntax for handling asynchronous operations more easily than with callbacks or promises.</li>
+    <li><b>Attribute:</b>Provides additional information about an HTML element.
+    </li>
+    <li><b>Block-Level Element:</b>An element that starts on a new line and takes up the full width available.</li>
+    <li><b>Box Model:</b>The model that represents the content, padding, border, and margin of an element.</li>
+    <li><b>Browser:</b>A software application used to access and display web pages.</li>
+    <li><b></b></li>
+     <li><b></b></li>
+     <li><b></b></li>
+    
+</ul>       
+    </main> 
+
+</body>
